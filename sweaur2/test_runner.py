5131f9101b92dbfd7968ebb0804b857f3e86c861
Toby White <tow@eaddrinu.se>
2011-02-10 23:37:49 +0000

    Add two more tests for obvious client credential failures

--- sweaur2/test_runner.py
+++ sweaur2/test_runner.py
@@ -7,15 +7,18 @@ from .processor import OAuth2Processor
 from .token_store import TokenStore
 
 
-class TestObviousFailures(object):
+class TestOAuth2Processor(object):
     def setUp(self):
         self.processor = OAuth2Processor(TokenStore(), ClientStore(), LowSecurityPolicy())
 
+
+class TestObviousFailures(TestOAuth2Processor):
     def testNoGrantType(self):
        try:
            self.processor.oauth2_token_endpoint()
        except InvalidRequest, e:
            assert e.error == 'invalid_request'
+           assert e.error_description == 'No grant_type specified'
        else:
            assert False
 
@@ -24,5 +27,26 @@ class TestObviousFailures(object):
            self.processor.oauth2_token_endpoint(grant_type='gimmegimme')
        except UnsupportedGrantType, e:
            assert e.error == 'unsupported_grant_type'
+           assert e.error_description == ''
        else:
            assert False
+
+
+class TestClientCredentialFailures(TestOAuth2Processor):
+    def testNoClientId(self):
+        try:
+            self.processor.oauth2_token_endpoint(grant_type='client_credentials', client_secret='SECRET')
+        except InvalidRequest, e:
+            assert e.error == 'invalid_request'
+            assert e.error_description == 'No client_id specified'
+        else:
+            assert False
+
+    def testNoClientSecret(self):
+        try:
+            self.processor.oauth2_token_endpoint(grant_type='client_credentials', client_id='ID')
+        except InvalidRequest, e:
+            assert e.error == 'invalid_request'
+            assert e.error_description == 'No client_secret specified'
+        else:
+            assert False
